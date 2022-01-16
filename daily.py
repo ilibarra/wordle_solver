fa5b15c3ea3b8e5760cc44dbd39fd96307c06361
ilibarra <ibarra.ignacio@gmail.com>
2022-01-17 00:11:23 +0100

    plotting log

--- daily.py
+++ daily.py
@@ -8,22 +8,28 @@ if __name__=='__main__':
     
     parser = argparse.ArgumentParser(description='Infer next best guess')
 
-    parser.add_argument('-g', '--guesses', nargs='+', required=True, help='a comma separate set of words tested')
+    parser.add_argument('-g', '--guesses', nargs='+', required=False, help='a comma separate set of words tested')
     parser.add_argument('-r', '--rules', required=False, help='Rules for the rest of words, in format 0 (gray, no match), 1 (yellow, word match) and 2 (green, position match) e.g. 00000,01002')
     parser.add_argument('-s', '--strategy', required=False, default='posfreqcovar',
                         help='strategy to use [wordfreq, posfreq posfreqcovar]')
     parser.add_argument('-d', '--dictionary', required=False, default='wordle_5',
                         help='word dictionary')
+    parser.add_argument('-p', '--previous', required=False, default='',
+                        help='previous guesses from previous days to discard')
 
     args = parser.parse_args()
     
     guesses = args.guesses[0].split(',')
     rules = args.rules.split(',')
+    previous = args.previous.split(',')
     
     assert len(guesses) == len(rules)
     
     words_by_dictionary, _ = wordle.load_dictionaries()
     remaining_words = words_by_dictionary[args.dictionary]
+    
+    if len(previous) > 0:
+        remaining_words = list(set(remaining_words) - set(previous))
     
     print('# words in dictionary:', len(remaining_words))
     
@@ -40,8 +46,8 @@ if __name__=='__main__':
     
     basename = str('-'.join([args.dictionary, "_".join(guesses), '_'.join(rules)]))
     out_basename = 'out/' + basename
-    print('Heatmaps saved at %s' % basename)
     wordle.plot_words(remaining_words, out_basename=out_basename, plot=True)
+    print('Heatmaps saved at out')
     
     guess = wordle.infer(remaining_words, args.strategy, sort_ascending=False)
     print('best guess (with strategy %s): %s' % (args.strategy, guess))
